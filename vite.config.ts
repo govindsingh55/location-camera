37135d5909177df41cabb8af882747ff921b4aca
govindsingh <govindsingh3091997@gmail.com>
2025-07-30 14:57:24 +0530

    remove HTTPS configuration from Vite server and add layout configuration for SSR and prerendering

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,9 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-import fs from 'fs';
-
 export default defineConfig({
   server: {
 	host: true,
-	https: fs.existsSync('cert/key.pem') && fs.existsSync('cert/cert.pem')
-	  ? {
-		  key: fs.readFileSync('cert/key.pem'),
-		  cert: fs.readFileSync('cert/cert.pem')
-		}
-	  : undefined
   },
   plugins: [tailwindcss(), sveltekit()]
 });
